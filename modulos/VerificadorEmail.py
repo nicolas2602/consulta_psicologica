d0a70652195828f2993627c853fdaf34d51854b0
Zywl <86253343+opZywl@users.noreply.github.com>
2023-08-22 17:41:22 -0300

    Create VerificadorEmail.py
    verificador de email

---
+++ modulos/VerificadorEmail.py
@@ -0,0 +1,34 @@
+class VerificadorEmail:
+    def __init__(self, email):
+        self.email = email
+
+    def verificar(self):
+        if self.email.count("@") != 1:
+            return "O email deve conter exatamente um '@'."
+
+        parts = self.email.split("@")
+        if len(parts) != 2:
+            return "Formato de email inválido."
+
+        if not parts[0] or not parts[1]:
+            return "O email deve conter um usuário e um domínio."
+
+        domain_parts = parts[1].split(".")
+        if len(domain_parts) < 2:
+            return "Domínio inválido após o '@'."
+
+        if not domain_parts[-1]:
+            return "O domínio deve ter pelo menos um caractere após o '@' e antes do '.'."
+
+        return None
+
+while True:
+    email = input("Digite o email: ")
+    verificador_email = VerificadorEmail(email)
+    erro_email = verificador_email.verificar()
+
+    if erro_email:
+        print(erro_email)
+    else:
+        print("Email válido!")
+        break
