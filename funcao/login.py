ff9499d1363bb17382951dccc9c76f925ab005f8
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2023-11-26 14:00:23 -0300

    arrumando

--- funcao/login.py
+++
@@ -1,6 +0,0 @@
-import funcao.conexao as con
-
-def login(usuario, senha):
-    sqlLogin = f"SELECT * FROM usuario WHERE nomeUsuario='{usuario}' AND senhaUsuario='{senha}';"
-    con.cursor.execute(sqlLogin)
-    con.conexao.commit()
--- versoes_anteriores/v1/teste.py
+++
@@ -1,6 +0,0 @@
-import funcao.conexao as con
-import funcao.cliente as cl
-
-sql = "SELECT * FROM cliente"
-resultado = cl.select(sql)
-print(resultado)
